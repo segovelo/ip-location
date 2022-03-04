d2a9e707b2bffd0a3bd7b832f613c00617104036
segovelo <gauchosinpingo@gmail.com>
2022-03-04 23:26:32 +0000

    Initial commit

---
+++ get-ip-location.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  4 23:20:07 2022
+
+@author: Segovelo
+"""
+
+import requests
+
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
+
+
+print(get_location())
